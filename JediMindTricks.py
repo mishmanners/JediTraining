038e2c5907183e1e93bda66343bafeab535afae3
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2023-05-11 12:09:38 +1000

    changetosimpler

--- JediMindTricks.py
+++ JediMindTricks.py
@@ -3,33 +3,22 @@
 import random
 
 # generate a sequence of 10 random numbers between 1 and 10 and print the result
-def generate_random_number():
-    random_number = random.randint(1,10)
-    return random_number
-    print("Your sequence is: " + random_number)
+numbers = [random.randint(1, 10) for i in range(10)]
+print(numbers)
 
-
-# once we have the sequence, multiply all the even numbers by 2, and add 5 to all the odd numbers
-def change_numbers(random_number):
-    if random_number % 2 == 0:
-        random_number = random_number * 2
+# once we have the sequence (numbers), multiply even numbers by 2, and add 5 to odd numbers
+for i in range(len(numbers)):
+    if numbers[i] % 2 == 0:
+        numbers[i] = numbers[i] * 2
     else:
-        random_number = random_number + 5
-    return random_number
-    # if the new number is greater than 10, replace it with 1
-    if random_number > 10:
-        random_number = 1
-    return random_number
+        numbers[i] = numbers[i] + 5
+        #print the result
+print(numbers)
+
+# if a number is great than 10, relace it with a 1
+for i in range(len(numbers)):
+    if numbers[i] > 10:
+        numbers[i] = 1
 
 # print the final new sequence to the console
-def print_final_sequence(random_number):
-    print("Your Jedi mind trick is number: " + random_number)
-
-# main function to run the code
-def main():
-    random_number = generate_random_number()
-    random_number = change_numbers(random_number)
-    print_final_sequence(random_number)
-
-# Uncomment the next line to test the code
-print("My Jedi Powers are weak")
+print("Your Jedi mind trick is number: " + numbers)

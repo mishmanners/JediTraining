6616ddc18803eaf439efc70013182ace260f85f3
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2023-05-11 12:16:46 +1000

    Works!

--- JediMindTricks.py
+++ JediMindTricks.py
@@ -4,7 +4,7 @@ import random
 
 # generate a sequence of 10 random numbers between 1 and 10 and print the result
 numbers = [random.randint(1, 10) for i in range(10)]
-print(numbers)
+print("Original sequence: " + str(numbers))
 
 # once we have the sequence (numbers), multiply even numbers by 2, and add 5 to odd numbers
 for i in range(len(numbers)):
@@ -13,12 +13,12 @@ for i in range(len(numbers)):
     else:
         numbers[i] = numbers[i] + 5
         #print the result
-print(numbers)
+print("Changed sequence: " + str(numbers))
 
-# if a number is great than 10, relace it with a 1
+# if a number in the new sequence is great than 10, relace it with a 1
 for i in range(len(numbers)):
     if numbers[i] > 10:
         numbers[i] = 1
 
 # print the final new sequence to the console
-print("Your Jedi mind trick is number: " + numbers)
+print("Your Jedi mind trick is number: " + str(numbers))

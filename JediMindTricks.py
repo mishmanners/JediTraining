94784eebcf18950d3d9a9047da04f3168a10d685
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2023-05-11 11:57:26 +1000

    change code to sequence

--- JediMindTricks.py
+++ JediMindTricks.py
@@ -1,42 +1,33 @@
-# write a function to generate a random number between 1 and 10
+# write a function to generate a sequence of 10 random numbers between 1 and 10
 
 import random
 
-# generate a random number between 1 and 10
+# generate a sequence of 10 random numbers between 1 and 10
 def generate_random_number():
-    return random.randint(1, 10)
+    random_number = random.randint(1, 10)
+    return random_number
+    print("Your sequence is:" + random_number)
 
-# once we have the random number, multiply even numbers by 2
-def multiply_even_numbers_by_two(random_number):
+# once we have the sequence, multiply all the even numbers by 2, and add 5 to all the odd numbers
+def change_numbers(random_number):
     if random_number % 2 == 0:
-        return random_number * 2
+        random_number = random_number * 2
     else:
-        return random_number
-
-# once we have the random number, add 5 to odd numbers
-def add_five_to_odd_numbers(random_number):
-    if random_number % 2 != 0:
-        return random_number + 5
-    else:
-        return random_number
-
-# once we have the random number, replace any number greater than 10 with 1
-def replace_numbers_greater_than_ten(random_number):
+        random_number = random_number + 5
+    return random_number
+    # if the new number is greater than 10, replace it with 1
     if random_number > 10:
-        return 1
-    else:
-        return random_number
+        random_number = 1
+    return random_number
 
-# print the final sequence to the console
+# print the final new sequence to the console
 def print_final_sequence(random_number):
     print("Your Jedi mind trick is number: " + random_number)
 
-# main function
+# main function to run the code
 def main():
     random_number = generate_random_number()
-    random_number = multiply_even_numbers_by_two(random_number)
-    random_number = add_five_to_odd_numbers(random_number)
-    random_number = replace_numbers_greater_than_ten(random_number)
+    random_number = change_numbers(random_number)
     print_final_sequence(random_number)
 
 # Uncomment the next line to test the code
